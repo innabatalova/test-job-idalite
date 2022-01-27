3121bdbc73f1f7bafe2d8eb98810b68d842a3822
innabatalova <batalovainna97@gmail.com>
2022-01-27 21:20:40 +0300

    Create: added mask for price input

--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,7 @@
 const validate = () => {
   const addBoardForm = document.querySelector(".add-board");
   const validityForm = addBoardForm.querySelectorAll(".add-val");
+  const addBoardNumber = addBoardForm.querySelector(".add-board__number");
   const addBoardButton = addBoardForm.querySelector(".add-board__button");
 
   //проверка валидности
@@ -14,36 +15,36 @@ const validate = () => {
   const checkValidity = () => {
     validityForm.forEach((item) => {
       if (!item.value) {
-        addBoardButton.classList.remove("add-board__button_create");
         item.classList.add("add-inval");
+        addBoardButton.classList.remove("add-board__button_create");
         let error = document.createElement("div");
         error.className = "error";
         error.innerHTML = "Поле является обязательным";
         item.parentElement.insertBefore(error, item);
       } else if (item.value.length < 6 && item.type == "text") {
-        addBoardButton.classList.remove("add-board__button_create");
         item.classList.add("add-inval");
+        addBoardButton.classList.remove("add-board__button_create");
         let error = document.createElement("div");
         error.className = "error";
         error.innerHTML = "Мин количество символов: 6";
         item.parentElement.insertBefore(error, item);
       } else if (item.value.length > 50 && item.type == "text") {
-        addBoardButton.classList.remove("add-board__button_create");
         item.classList.add("add-inval");
+        addBoardButton.classList.remove("add-board__button_create");
         let error = document.createElement("div");
         error.className = "error";
         error.innerHTML = "Макс количество символов: 50";
         item.parentElement.insertBefore(error, item);
       } else if (!item.value.replace(/[\s]+/g, "")) {
-        addBoardButton.classList.remove("add-board__button_create");
         item.classList.add("add-inval");
+        addBoardButton.classList.remove("add-board__button_create");
         let error = document.createElement("div");
         error.className = "error";
         error.innerHTML = "Введите корректное значение";
         item.parentElement.insertBefore(error, item);
       } else {
-        console.log(addBoardButton);
         addBoardButton.classList.add("add-board__button_create");
+        addBoardButton.disabled = false;
       }
     });
   };
@@ -61,6 +62,17 @@ const validate = () => {
       item.classList.remove("add-inval");
     });
   };
+
+  //маска разделения тысячных пробелом для поля цены
+  addBoardNumber.addEventListener("keypress", () => {
+    console.log(addBoardNumber);
+    if (addBoardNumber.value.length > 2) {
+      var str = addBoardNumber.value;
+      let newStr = str.replace(/(\d\d)(?=(\d\d)+([^\d]|$))/g, "$1 ");
+      console.log(newStr);
+      addBoardNumber.value = newStr;
+    }
+  });
 };
 
 validate();
